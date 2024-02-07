0965c92804d5348e2d20d947552a086eb498064d
Filip Stojanovic <philsty92@gmail.com>
2024-02-07 07:22:05 -0500

    Added contact form.

--- components/Contact.js
+++ components/Contact.js
@@ -3,6 +3,9 @@
 // styles
 import styles from './Contact.module.scss'
 
+// components
+import Button from './Button'
+
 // map
 import Map, { Marker } from 'react-map-gl'
 import 'mapbox-gl/dist/mapbox-gl.css'
@@ -21,17 +24,53 @@ const Contact = ({ content }) => {
 						mapStyle='mapbox://styles/filip-desophy/clsbnuc4101jq01ped1nqcqs5'
 						mapboxAccessToken={process.env.mapboxToken}
 						scrollZoom={false}
+						attributionControl={false}
 					>
 						<Marker
 							longitude={content.fields.longitude}
 							latitude={content.fields.latitude}
-							// color='#ffd500'
 						>
-							<img src='/pin.svg' alt='marker' />
+							<img
+								src='/pin.svg'
+								alt='marker'
+								style={{ paddingBottom: '20px' }}
+							/>
 						</Marker>
 					</Map>
 				</div>
-				<div className={styles.form}>Form</div>
+				<div className={styles.form}>
+					<h2>{content.fields.title}</h2>
+
+					<form
+						name='contact'
+						method='POST'
+						netlify-honeypot='bot-field'
+						data-netlify='true'
+						// action='/success'
+					>
+						<input type='hidden' name='form-name' value='contact' />
+
+						<input
+							type='text'
+							id='fullName'
+							name='fullName'
+							placeholder='Full Name'
+						/>
+
+						<input
+							type='text'
+							id='contact'
+							name='contact'
+							placeholder='Contact'
+						/>
+
+						<textarea id='message' name='message' placeholder='Message..' />
+
+						<button type='submit'>
+							<Button color='yellow' text='Send' icon='/arrow.svg' animate />
+						</button>
+					</form>
+				</div>
 			</div>
 		</section>
 	)
