6d2c91a6994acf552aabd9dd9d2f90bed0da0877
Filip Stojanovic <philsty92@gmail.com>
2024-09-18 13:10:53 -0400

    Reverted to latest Next JS.

--- app/success/page.js
+++ app/success/page.js
@@ -3,7 +3,7 @@ export const dynamic = 'force-static'
 const SuccessPage = () => {
 	return (
 		<div style={{ padding: '2rem', textAlign: 'center' }}>
-			<h1>Thank You!</h1>
+			<h1>Thank You!!!</h1>
 			<p>Your message has been sent successfully.</p>
 		</div>
 	)
